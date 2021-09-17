17caf7e15dcf1a53950bce349947f0e29d749525
charlie currid <charlie.currid@student.educationn.wa.edu.au>
2021-09-17 12:31:22 +0800

    Steps 2-4 done

--- main.py
+++ main.py
@@ -2,8 +2,24 @@
 
 import pygame                             # Imports pygame and other libraries
 import random
-# Define Classes (sprites) here
+class FallingObject(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.timecreated= pygame.time.get_ticks()
+        self.image= pygame.Surface([30,30])
+        self.image.set_colorkey(black)
 
+        self.rect= self.image.get_rect()
+        self.rect.x= random.randint(0,670)
+        self.rect.y= 0
+
+    def setImage(self,graphicSelected):
+        fallingObjectsImage= pygame.image.load(graphicSelected)
+        self.image.blit(fallingObjectsImage,(0,0))
+
+    def moveFallingObjects(self,distance):
+        if self.rect.y <= 470:
+            self.rect.y= self.rect.y + distance
 pygame.init()                               # Pygame is initialised (starts running)
 
 screen = pygame.display.set_mode([700,500]) # Set the width and height of the screen [width,height]
@@ -15,7 +31,9 @@ black    = (   0,   0,   0)                 # Define some colors using rgb value
 white    = ( 255, 255, 255)                 # used throughout the game instead of using rgb values.
 
 # Define additional Functions and Procedures here
+allFallingObjects= pygame.sprite.Group()
 
+nextApple= pygame.time.get_ticks() + 2500
 # -------- Main Program Loop -----------
 while done == False:
 
@@ -24,7 +42,17 @@ while done == False:
             done = True                     # Flag that we are done so we exit this loop
 
     # Update sprites here
+    if pygame.time.get_ticks() > nextApple:
+        nextObject= FallingObject()
+        nextObject.setImage('Apple.png')
+        allFallingObjects.add(nextObject)
+        nextApple= pygame.time.get_ticks() + 15
+
+    for eachObject in (allFallingObjects.sprites()):
+        eachObject.moveFallingObjects(5)
+
     screen.blit(background_image, [0,0])
+    allFallingObjects.draw(screen)
     pygame.display.flip()                   # Go ahead and update the screen with what we've drawn.
     clock.tick(20)                          # Limit to 20 frames per second
 
